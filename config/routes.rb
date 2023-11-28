1e598edf11ffe958b55966fa08006f79a7bd0f9f
ecurzola <146590441+ecurzola@users.noreply.github.com>
2023-11-28 07:31:54 +0000

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "misc", :action => "homepage" })
+  get("/users", { :controller => "", :action => "" })
+  get("/photos", { :controller => "", :action => "" })
+
 end
