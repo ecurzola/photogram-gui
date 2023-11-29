63a0dcd9aa26aa8f6443439ecd71e40eda566dac
ecurzola <146590441+ecurzola@users.noreply.github.com>
2023-11-29 05:44:42 +0000

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index" })
+  get("/users/:path_username", { :controller => "users", :action => "show" })
+  
+  
+  
   get("/photos", { :controller => "", :action => "" })
 
 end
