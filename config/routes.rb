da1dec9792df88f110215da8d6dc887ae5252cb9
ecurzola <146590441+ecurzola@users.noreply.github.com>
2023-11-29 07:05:10 +0000

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "users", :action => "index" })
+
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show" })
   
@@ -7,6 +9,6 @@ Rails.application.routes.draw do
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:path_id", { :controller => "photos", :action => "show" })
   get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
-
+  post("/insert_photo_record", { :controller => "photos", :action => "create" })
 
 end
